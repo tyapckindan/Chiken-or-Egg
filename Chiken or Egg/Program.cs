9da32728492afaaff30582756ec860c1c28d74ea
Данила Тяпкин <dtapkin6@gmail.com>
2023-09-15 10:48:28 +0300

    Добавьте файлы проекта.

---
+++ Chiken or Egg/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void MyThreadChicken()
+    {
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Курица");
+    }
+
+    static void MyThreadAnswer(Thread chicken)
+    {
+        Thread.Sleep(1);
+        bool ans = chicken.IsAlive;
+        Console.WriteLine(ans ? "Спор Окончен. Победила Курица." : "Спор окончен. Победило Яйцо.");
+    }
+    private static void Main(string[] args)
+    {
+        Thread chicken = new(MyThreadChicken);
+        chicken.Start();
+
+        for (int i = 0; i < 100; i++)
+            Console.WriteLine("Яйцо");
+
+        MyThreadAnswer(chicken);
+    }
+}
